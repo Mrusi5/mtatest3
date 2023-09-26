5b8a43fcc05fe66eba9f5a7c91a8c696ffe30f2b
Mrusi5 <113948511+Mrusi5@users.noreply.github.com>
2023-09-26 14:47:33 +0400

    Update Smanage_faction.lua

--- factions/Smanage_faction.lua
+++ factions/Smanage_faction.lua
@@ -1,18 +1,15 @@
 
-
-
 --Назначения лидера фракции
-
 function setFactionLeader( player, playerID, factionID )
     local playerTarget = getPlayerByID( playerID )
     
-    if factions[ factionID ] and playerTarget then
+    if FACTIONS[ factionID ] and playerTarget then
         local currentFaction = getPlayerFaction( playerTarget )
         if not currentFaction or currentFaction == factionID then
-            factions[ factionID ].leader = playerTarget
+            FACTIONS[ factionID ].leader = playerTarget
 
-            if not table.contains( factions[ factionID ].members, playerTarget ) then
-                table.insert( factions[ factionID ].members, playerTarget )
+            if not table.contains( FACTIONS[ factionID ].members, playerTarget ) then
+                table.insert( FACTIONS[ factionID ].members, playerTarget )
             end
 
             outputChatBox( getPlayerName( playerTarget ) .. " назначен лидером фракции " .. factionID, player )
@@ -24,20 +21,15 @@ function setFactionLeader( player, playerID, factionID )
     end
 end
 
-
 ---Вступление в организацию или выход из неё если не указанно id фракции
-
 function setPlayerFaction( player, playerID, factionID )
     local playerTarget = getPlayerByID( playerID )
     local currentFaction = getPlayerFaction( playerTarget )
 
-
-
 ---Вступление     
-
-    if factions[ factionID ] and playerTarget then
+    if FACTIONS[ factionID ] and playerTarget then
         if not currentFaction then
-            table.insert( factions[ factionID ].members, playerTarget )
+            table.insert( FACTIONS[ factionID ].members, playerTarget )
             outputChatBox( getPlayerName( playerTarget ) .. " вступил(a) в фракцию " .. factionID, player )
         elseif currentFaction == factionID then
             outputChatBox( getPlayerName( playerTarget ) .. " уже состоит в этой фракции", player )
@@ -45,17 +37,14 @@ function setPlayerFaction( player, playerID, factionID )
             outputChatBox( getPlayerName( playerTarget ) .. " уже состоит в другой фракции", player )
         end
 
-
-
 ----Выход      
-
     elseif not factionID and playerID then
         if currentFaction then
-            local faction = factions[ currentFaction ]
-            for i, member in ipairs( faction.members ) do
+            local faction = FACTIONS[ currentFaction ]
+            for i, member in pairs( faction.members ) do
                 if member == playerTarget then
-                    if playerTarget == factions[ currentFaction ].leader then
-                        factions[ currentFaction ].leader = nil
+                    if playerTarget == FACTIONS[ currentFaction ].leader then
+                        FACTIONS[ currentFaction ].leader = nil
                         outputChatBox( getPlayerName( playerTarget ) .. " снят с поста лидера " .. currentFaction, player )
                     end
                     table.remove( faction.members, i ) 
@@ -64,20 +53,19 @@ function setPlayerFaction( player, playerID, factionID )
                 end
             end
         else
-            outputChatBox( getPlayerName( playerTarget ) .. " не состоите в фракции", player )
+            if playerTarget then
+                outputChatBox( getPlayerName( playerTarget ) .. " не состоит в фракции", player )
+            end
         end
     else
         outputChatBox( "Игрока с таким ID нет", player )
     end
 end
 
-
-
 ---Получение фракции игрока 
-
 function getPlayerFaction( player )
 
-    for factionID, factionData in pairs( factions ) do
+    for factionID, factionData in pairs( FACTIONS ) do
         if table.contains( factionData.members, player ) then
             return factionID
         end
@@ -85,13 +73,10 @@ function getPlayerFaction( player )
     return nil
 end
 
-
-
 ---Получение userdata игрока по ID игрока
-
 function getPlayerByID( playerID )
     local players = getElementsByType( "player" )
-    for _, player in ipairs( players ) do
+    for _, player in pairs( players ) do
         local storedID = getElementData( player, "playerID" )
         if storedID == tonumber( playerID ) then
             return player
@@ -99,8 +84,6 @@ function getPlayerByID( playerID )
     end
     return nil
 end
-
-
 
 addCommandHandler( "set_player_faction_leader", function( player, cmd, targetPlayerID, factionID )
     
@@ -111,9 +94,6 @@ addCommandHandler( "set_player_faction_leader", function( player, cmd, targetPla
     end
 end )
 
-
-
-
 addCommandHandler( "set_player_faction", function( player, cmd, targetPlayerID, factionID )
     
     if targetPlayerID then
@@ -122,8 +102,6 @@ addCommandHandler( "set_player_faction", function( player, cmd, targetPlayerID, 
         outputChatBox( "Игрок не найден", player )
     end
 end )
-
-
 
 function table.contains( table, element )
     for _, value in pairs( table ) do

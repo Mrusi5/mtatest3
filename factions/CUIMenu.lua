fb79246242d42396c8557cfcd51fb85890bda91b
Mrusi5 <113948511+Mrusi5@users.noreply.github.com>
2023-09-26 14:46:42 +0400

    Update CUIMenu.lua

--- factions/CUIMenu.lua
+++ factions/CUIMenu.lua
@@ -1,6 +1,3 @@
-
-
-
 ----------------------------------------------Отрисовка панели------------------------------------------------------
 
 local screenWidth, screenHeight = guiGetScreenSize( )
@@ -14,89 +11,112 @@ local playerFactionData = nil
 local accessLevelplayer = nil
 
 
-local orgPanel = guiCreateWindow( x, y, panelWidth, panelHeight, "Панель организации", false )
-local orgName = "Название организации"
-guiSetVisible( orgPanel, false )
-
-local tabPanel = guiCreateTabPanel(0.05, 0.1, 0.9, 0.8, true, orgPanel )
-local tabMembers = guiCreateTab( "Список участников", tabPanel )
-local tabCity = guiCreateTab( "Управление городом", tabPanel )
-
-local gridlist = guiCreateGridList( 0.05, 0.05, 0.9, 0.8, true, tabMembers )
-guiGridListAddColumn( gridlist, "ID", 0.2 )
-guiGridListAddColumn( gridlist, "Имя", 0.5 )
-
-local fireButton = guiCreateButton( 0.05, 0.9, 0.2, 0.05, "Уволить", true, tabMembers )
-local invButton = guiCreateButton( 0.75, 0.9, 0.2, 0.05, "Пригласить", true, tabMembers )
-local inviteEdit = guiCreateEdit( 0.64, 0.9, 0.1, 0.05, "", true, tabMembers )
----------------------------------------------------------------------------------------------------------------------
+local orgPanel = nil
 
 ----------------------------------------Получение данных фракции-----------------------------------------------------
-
 addEvent( "factionDataReceived", true )
 addEventHandler( "factionDataReceived", root, function( factionData )
     playerFactionData = factionData
 end )
-
----------------------------------------------------------------------------------------------------------------------
-
 --------------------------------------------Видимость панели---------------------------------------------------------
-
 function toggleOrgPanel( )
     requestPlayerFaction( )
-    triggerServerEvent("requestFactionData", localPlayer, localPlayer )
-    updateGridList( )
-    if accessLevelplayer then
-        isOrgPanelVisible = not isOrgPanelVisible
-        guiSetVisible( orgPanel, isOrgPanelVisible )
-        showCursor( isOrgPanelVisible )
-    end
+    triggerServerEvent( "requestFactionData", localPlayer, localPlayer )
+    setTimer( function( )
+        if accessLevelplayer then
+            if not orgPanel then
+                orgPanel = guiCreateWindow( x, y, panelWidth, panelHeight, "Панель организации", false )
+                guiSetVisible( orgPanel, true )
+                showCursor( true )
+                
+                orgName = "Название организации"
+        
+        
+                tabPanel = guiCreateTabPanel( 0.05, 0.1, 0.9, 0.8, true, orgPanel )
+                tabMembers = guiCreateTab( "Список участников", tabPanel )
+        
+                gridlist = guiCreateGridList( 0.05, 0.05, 0.9, 0.8, true, tabMembers )
+                guiGridListAddColumn( gridlist, "ID", 0.2 )
+                guiGridListAddColumn( gridlist, "Имя", 0.5 )
+    
+                if accessLevelplayer == "leader" then
+                    tabCity = guiCreateTab( "Управление городом", tabPanel )
+                    fireButton = guiCreateButton( 0.05, 0.9, 0.2, 0.05, "Уволить", true, tabMembers )
+                    invButton = guiCreateButton( 0.75, 0.9, 0.2, 0.05, "Пригласить", true, tabMembers )
+                    inviteEdit = guiCreateEdit( 0.64, 0.9, 0.1, 0.05, "", true, tabMembers )
+--------------------------------------------Кнопка "Уволить"--------------------------------------------------------
+                    addEventHandler( "onClientGUIClick", fireButton, function( )
+                        local selectedRow, selectedCol = guiGridListGetSelectedItem( gridlist )
+                        
+                        if selectedRow and selectedCol then
+                            local playerID = guiGridListGetItemText( gridlist, selectedRow, 1 )
+                            if playerID ~= "" then
+                                triggerServerEvent( "firePlayerFromFaction", root, localPlayer, playerID )
+                            else
+                                outputChatBox( "Выберите игрока из списка, чтобы уволить его.", 255, 0, 0 )
+                            end
+                    
+                        end
+                    end, false )
+                    -----------------------------------------Кнопка "Пригласить"--------------------------------------------------------
+                    function sendInvite( )
+                        local playerID = guiGetText( inviteEdit )
+                        local factionName = playerFactionData.name
+                        triggerServerEvent( "checkID", root, localPlayer, playerID, factionName )
+                    end
+                    
+                    addEventHandler( "onClientGUIClick", invButton, sendInvite, false )
+                    updateGridList()
+                end
+            else
+                orgPanel = nil
+                showCursor(false)
+            end
+        end
+    end, 100, 1 )
 end
-
 
 bindKey( "p", "down", toggleOrgPanel )
 
 function requestPlayerFaction( )
     triggerServerEvent( "checkPlayerFaction", localPlayer, localPlayer )
 end 
-
-
-
 -- Проверка доступа панели
-
 addEvent( "receiveFactionForAccessLevel", true )
 addEventHandler( "receiveFactionForAccessLevel", root, function( accessLevel )
     if accessLevel then
         if accessLevel == "member" then
+            if orgPanel then
+                destroyElement(orgPanel)
+                showCursor(false)
+            end
             accessLevelplayer = "member"
-            guiSetVisible( invButton, false )
-            guiSetVisible( fireButton, false )
-            guiSetVisible( tabCity, false )
-            guiSetVisible( inviteEdit, false )
         elseif accessLevel == "leader" then
+            if orgPanel then
+                destroyElement(orgPanel)
+                showCursor(false)
+            end
             accessLevelplayer = "leader"
-            guiSetVisible( inviteEdit, true )
-            guiSetVisible( invButton, true )
-            guiSetVisible( fireButton, true )
-            guiSetVisible( tabCity, true )
         end
     else
         accessLevelplayer = nil
+        if orgPanel then
+            destroyElement(orgPanel)
+            showCursor(false)
+            orgPanel = nil
+        end
         outputChatBox( "Вы не состоите в фракции." )
     end
 end )
 
---------------------------------------------------------------------------------------------------------------------
-
 ----------------------------------------Заполнение списка участников------------------------------------------------
-
 function updateGridList( )
     guiGridListClear( gridlist )
 
 
     local members = playerFactionData.members
 
-    for i, memberID in ipairs( members ) do
+    for i, memberID in pairs( members ) do
         local memberName = getPlayerName( memberID ) or "N/A"
         local memberID = getElementData( memberID, "playerID" ) or "N/A"
 
@@ -107,58 +127,7 @@ function updateGridList( )
     end
 end
 
---------------------------------------------------------------------------------------------------------------------
-
---------------------------------------------Кнопка "Уволить"--------------------------------------------------------
-
-addEventHandler( "onClientGUIClick", fireButton, function( )
-    local selectedRow, selectedCol = guiGridListGetSelectedItem( gridlist )
-    
-    if selectedRow and selectedCol then
-        local playerID = guiGridListGetItemText( gridlist, selectedRow, 1 )
-        if playerID ~= "" then
-            triggerServerEvent( "firePlayerFromFaction", root, localPlayer, playerID )
-        else
-            outputChatBox( "Выберите игрока из списка, чтобы уволить его.", 255, 0, 0 )
-        end
-
-    end
-end, false )
-
---------------------------------------------------------------------------------------------------------------------
-
------------------------------------------Кнопка "Пригласить"--------------------------------------------------------
-local targetPlayer = false
-local inviteCooldowns = {}
-local inviteCooldown = 60 * 1000
-
-
-
---Отправка приглашения
-
-function sendInvite( )
-    local playerID = guiGetText( inviteEdit )
-    local factionName = playerFactionData.name
-    local lastInviteTime = inviteCooldowns[ playerID ]
-
-
-
----- Проверяем что игрок отправляет приглашение впервые или отправлял более 1 мин назад
-
-    if not lastInviteTime or getTickCount( ) - lastInviteTime >= inviteCooldown then
-        inviteCooldowns[ playerID] = getTickCount( )
-        triggerServerEvent( "checkID", root, localPlayer, playerID, factionName )
-    else
-        outputChatBox( "Вы можете отправить инвайт этому игроку только раз в 1 минуту.", 255, 0, 0 )
-    end
-end
-
-addEventHandler( "onClientGUIClick", invButton, sendInvite, false )
-
 local inviteWindow = nil
-
-
-
 --Окно приглашения
 
 function receiveInvite( inviteMessage, factionIdforInvite )
@@ -169,11 +138,7 @@ function receiveInvite( inviteMessage, factionIdforInvite )
         local declineButton = guiCreateButton( 0.5, 0.7, 0.3, 0.2, "Отказаться", true, inviteWindow )
 
         guiSetInputEnabled( true )
-
-
-
 ---------Обработка нажатия кнопки "Принять"
-
         addEventHandler( "onClientGUIClick", acceptButton, function( )
 
             local playerID = getElementData( localPlayer, "playerID" )
@@ -184,11 +149,7 @@ function receiveInvite( inviteMessage, factionIdforInvite )
 
             guiSetInputEnabled( false )
         end, false )
-
-
-
 ---------Обработка нажатия кнопки "Отказаться"
-
         addEventHandler( "onClientGUIClick", declineButton, function( )
             destroyElement( inviteWindow )
 
@@ -197,10 +158,5 @@ function receiveInvite( inviteMessage, factionIdforInvite )
     end
 end
 
-
-
 addEvent( "onReceiveInvite", true )
 addEventHandler( "onReceiveInvite", resourceRoot, receiveInvite )
-
-
---------------------------------------------------------------------------------------------------------------------
